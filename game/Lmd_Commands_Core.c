1ca4feb897046630dfa248e7ea40fba5158f1d74
iomatix <mateuszwypchlak@gmail.com>
2019-05-27 14:33:03 +0200

    Update version

--- game/Lmd_Commands_Core.c
+++ game/Lmd_Commands_Core.c
@@ -265,7 +265,7 @@ void Cmd_Help_f(gentity_t *ent){
 		}
 	}
 	else {
-		Disp(ent, va("^2Lugormod %s -- ^8by iomatix\n", Lugormod_Version.string));
+		Disp(ent, va("^8Lugormod %s -- ^0by iomatix\n ^2Guide: www.github.com/iomatix/Lugormod-X\n", Lugormod_Version.string));
 		Cmd_Help_ListCategories(ent);
 	}
 }
